22ef0c3ac237f92e8aa1c56659f72944b2738c5c
Md. Abid Hossain <abidhossain392@gmail.com>
2021-09-17 23:02:23 +0600

    files added

---
+++ Code/mcu_code_arduino.ino
@@ -0,0 +1,128 @@
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SoftwareSerial.h>
+#include <SimpleTimer.h>
+
+char auth[] = "nSC5994bDxGyYpYM4ds3sFeKoDSHBdKz";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "ABID";
+char pass[] = "8901234567";
+
+SimpleTimer timer;
+
+String myString; // complete message from arduino, which consistors of snesors data
+char rdata; // received charactors
+
+float firstVal, secondVal,thirdVal,forthVal; // 
+// This function sends Arduino's up time every second to Virtual Pin (1).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V1, millis() / 1000);
+  
+}
+
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+
+    timer.setInterval(1000L,sensorvalue1); 
+     timer.setInterval(1000L,sensorvalue2); 
+     timer.setInterval(1000L,sensorvalue3);
+      timer.setInterval(1000L,sensorvalue4);
+}
+
+void loop()
+{
+   if (Serial.available() == 0 ) 
+   {
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+   }
+   
+  if (Serial.available() > 0 ) 
+  {
+    rdata = Serial.read(); 
+    myString = myString+ rdata; 
+   // Serial.print(rdata);
+    if( rdata == '\n')
+    {
+String l = getValue(myString, ',', 0);
+String m = getValue(myString, ',', 1);
+String n = getValue(myString, ',', 2); 
+String o = getValue(myString, ',', 3); 
+
+firstVal = l.toFloat();
+secondVal = m.toFloat();
+thirdVal = n.toFloat();
+forthVal = o.toFloat();
+
+  myString = "";
+// end new code
+    }
+  }
+
+}
+
+void sensorvalue1()
+{
+float sdata = firstVal;
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V2, sdata);
+
+}
+void sensorvalue2()
+{
+float sdata = secondVal;
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V3, sdata);
+
+}
+
+void sensorvalue3()
+{
+float sdata = thirdVal;
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V4, sdata);
+
+}
+void sensorvalue4()
+{
+float sdata = forthVal;
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, sdata);
+
+}
+
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
